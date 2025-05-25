fa9433199a6fd9cb4e6f1320d6fbfc67b772502c
오다혜 <devohda123@gmail.com>
2025-05-26 01:58:49 +0900

    Merge commit 'b3166fd287eef4c4cd09aa65cc9a4ea263469627'

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,5 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer U> ? 
+                        U extends Promise<any> ?
+                            MyAwaited<U> 
+                            : U 
+                    : never
